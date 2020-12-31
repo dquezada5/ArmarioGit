ff70a080ed45581253ef4c6f0800eb29854eef9a
dquezada5 <fer1603qp@gmail.com>
2020-12-30 20:35:41 -0500

    Generacion de entidades

--- src/Entidades/Persona.java
+++ src/Entidades/Persona.java
@@ -1,5 +1,37 @@
 package Entidades;
 
 public class Persona {
+private String nombre;
+private int edad;
+private String apellido;
+/**
+ * @param nombre
+ * @param edad
+ * @param apellido
+ */
+public Persona(String nombre, int edad, String apellido) {
+	super();
+	this.nombre = nombre;
+	this.edad = edad;
+	this.apellido = apellido;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public int getEdad() {
+	return edad;
+}
+public void setEdad(int edad) {
+	this.edad = edad;
+}
+public String getApellido() {
+	return apellido;
+}
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
 
 }
